93584210285fb0c079eb0628cd3f1d98eadd4aae
Shahla Atiq <shahlatiq24@gmail.com>
2019-10-08 22:30:35 +0100

    please check

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -2,6 +2,37 @@ function addVAT(price) {
     return price + (0.2 * price);
 }
 
+
+
+function getFullName(a,b) {
+    const fullName = ("Haruki " + "Murakami")
+    return fullName
+
+}
+
+function makeHalfPrice(price)  {
+    return price - (0.5 * price)
+}
+
+function countBooks(number)  {
+const books = ("Alpha", "Bravo", "Charlie")
+return number(length[2])
+
+}
+
+
+function countBooks(number)  {
+    const books = ("Alpha", "Bravo", "Charlie")
+    return number(books.length[2]);
+    
+    }
+
+
+
+
+
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice
 };
--- test/utils.test.js
+++ test/utils.test.js
@@ -8,4 +8,35 @@ describe("utils.addVAT", () => {
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
   });
-});
+
+  test("returns 118.8 when passed 99", () => {
+    expect(utils.addVAT(99)).toBe(118.8);
+  });
+  test("returns 240 when passed 200", () => {
+    expect(utils.addVAT(200)).toBe(240);
+  });
+
+  describe("utils.getFullName", () => {
+    test("returns Haruki Murakami when passed Haruki and Murakami", () => {
+      expect(utils.getFullName("Haruki", "Murakami")).toBe("Haruki Murakami");
+    });
+  });
+
+  describe("utils.makeHalfPrice", () => {
+    test("returns 50 when passed 100", () => {
+      expect(utils.makeHalfPrice(100)).toBe(50);
+    });
+    });
+
+    describe("utils.getFullName", () => {
+      test("returns Haruki Murakami when passed Haruki and Murakami", () => {
+        expect(utils.getFullName("Haruki", "Murakami")).toBe("Haruki Murakami");
+      });
+    });
+
+
+
+
+
+    
+  });
